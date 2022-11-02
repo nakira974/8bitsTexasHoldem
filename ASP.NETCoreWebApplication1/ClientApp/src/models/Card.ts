b9f8afe5e3d78f0a7db1dd2ec9d3fb29e77796a3
nakira974 <maxime.loukhal@gmail.com>
2022-11-02 22:33:39 +0100

    rename SQUARE_*.png to DIAMOND_*.png, update documentation

--- ASP.NETCoreWebApplication1/ClientApp/src/models/Card.ts
+++ ASP.NETCoreWebApplication1/ClientApp/src/models/Card.ts
@@ -6,14 +6,23 @@ import {AssetBase} from "./AssetBase";
  * @description Describe the family of a card 
  **/
 export enum CardType{
-    /**@see https://fr.wikipedia.org/wiki/Pique_(carte_%C3%A0_jouer)*/
+    
+    /**@description The club (♣) is a playing card sign, one of four French signs with heart, tile and spade.
+     * @see https://fr.wikipedia.org/wiki/Tr%C3%A8fle_(carte_%C3%A0_jouer)*/
     Club = 0x0EAB0A,
-    /**@see https://fr.wikipedia.org/wiki/C%C5%93ur_(carte_%C3%A0_jouer)*/
+    
+    /**@description The heart (♥) is a playing card sign, one of the four French signs with spade, tile and clover.
+     * @see https://fr.wikipedia.org/wiki/C%C5%93ur_(carte_%C3%A0_jouer)*/
     Heart = 0x0EAB0B,
-    /**@see https://fr.wikipedia.org/wiki/Tr%C3%A8fle_(carte_%C3%A0_jouer)*/
-    Spade = 0x0EAB0C,
-    /**@see https://fr.wikipedia.org/wiki/Carreau_(carte_%C3%A0_jouer)*/
-    Square = 0x0EAB0D,
+    
+   /** @description The spade (♠) is a playing card sign, one of the four French signs with heart, tile and clover.
+    * @see https://fr.wikipedia.org/wiki/Pique_(carte_%C3%A0_jouer)*/
+   Spade = 0x0EAB0C,
+    
+    /**@description The diamond (♦) is a playing card sign, one of four French signs with spades, hearts and clubs.
+     * @see https://fr.wikipedia.org/wiki/Carreau_(carte_%C3%A0_jouer)*/
+    Diamond = 0x0EAB0D,
+    
     /**Cards like reversed side or the joker*/ 
     Other = 0x0EAB0E
 }
@@ -25,7 +34,7 @@ export const AllowedCardTypes : CardType[]  = [
     CardType.Club,
     CardType.Heart,
     CardType.Spade,
-    CardType.Square 
+    CardType.Diamond 
 ];
 
 /**
@@ -34,35 +43,64 @@ export const AllowedCardTypes : CardType[]  = [
  * @description Describe the number of a card
  **/
 export enum CardNumber{
+    
     /** Reversed side of cards */
     ReverseFace = 0x00,
-    /**@see https://fr.wikipedia.org/wiki/Deux_(carte_%C3%A0_jouer)*/
+    
+    /**@description Two is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Deux_(carte_%C3%A0_jouer)*/
     Two= 0x02,
-    /**@see https://fr.wikipedia.org/wiki/Trois_(carte_%C3%A0_jouer)*/
+    
+    /**@description Three is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Trois_(carte_%C3%A0_jouer)*/
     Three = 0x03,
-    /**@see https://fr.wikipedia.org/wiki/Quatre_(carte_%C3%A0_jouer)*/
+    
+    /**@description Four is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Quatre_(carte_%C3%A0_jouer)*/
     Four = 0x04,
-    /**@see https://fr.wikipedia.org/wiki/Cinq_(carte_%C3%A0_jouer)*/
+    
+    /**@description Five is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Cinq_(carte_%C3%A0_jouer)*/
     Five= 0x05,
-    /**@see https://fr.wikipedia.org/wiki/Six_(carte_%C3%A0_jouer)*/
+    
+    /**@description Six is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Six_(carte_%C3%A0_jouer)*/
     Six= 0x06,
-    /**@see https://fr.wikipedia.org/wiki/Sept_(carte_%C3%A0_jouer)*/
+    
+    /**@description Seven is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Sept_(carte_%C3%A0_jouer)*/
     Seven = 0x07,
-    /**@see https://fr.wikipedia.org/wiki/Huit_(carte_%C3%A0_jouer)*/
+    
+    /**@description Height is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Huit_(carte_%C3%A0_jouer)*/
     Height = 0x08,
-    /**@see https://fr.wikipedia.org/wiki/Neuf_(carte_%C3%A0_jouer)*/
+    
+    /**@description Nine is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Neuf_(carte_%C3%A0_jouer)*/
     Nine= 0x09,
-    /**@see https://fr.wikipedia.org/wiki/Dix_(carte_%C3%A0_jouer)*/
+    
+    /**@description Ten is a playing card value
+     * @see https://fr.wikipedia.org/wiki/Dix_(carte_%C3%A0_jouer)*/
     Ten = 0x0A,
-    /**@see https://fr.wikipedia.org/wiki/Valet_(carte_%C3%A0_jouer)*/
+    
+    /**@description The valet is a playing card figure. Usually representing a young man, it is often the weakest figure
+     * @see https://fr.wikipedia.org/wiki/Valet_(carte_%C3%A0_jouer)*/
     Jack = 0x0B,
-    /**@see https://fr.wikipedia.org/wiki/Dame_(carte_%C3%A0_jouer)*/
+    
+    /**@description The lady or queen is a playing card figure, usually representing a noble woman
+     * @see https://fr.wikipedia.org/wiki/Dame_(carte_%C3%A0_jouer)*/
     Queen = 0x0C,
-    /**@see https://fr.wikipedia.org/wiki/Roi_(carte_%C3%A0_jouer)*/
+    
+    /**@description The king is a playing card figure, usually representing a noble man
+     * @see https://fr.wikipedia.org/wiki/Roi_(carte_%C3%A0_jouer)*/
     King = 0x0D,
-    /**@see https://fr.wikipedia.org/wiki/As_(carte_%C3%A0_jouer)*/
+    
+    /**@description The ace is a playing card value, corresponding to the number 1
+     * @see https://fr.wikipedia.org/wiki/As_(carte_%C3%A0_jouer)*/
     Ace = 0X0E,
-    /**@see https://fr.wikipedia.org/wiki/Joker_(carte_%C3%A0_jouer)*/
+    
+    /**@description The joker is a «generic» card capable of representing one of the other playing cards
+     * @see https://fr.wikipedia.org/wiki/Joker_(carte_%C3%A0_jouer)*/
     Joker = 0XFFFFFFFFFFFF
 }
 
@@ -94,7 +132,7 @@ export const AllowedCardNumbers : CardNumber[] = [
 export class Card extends AssetBase{
 
     /**
-     * @description Get the family (Ace, Spade...) of the current card
+     * @description Get the family (Club, Spade...) of the current card
      */
     get family(): CardType {
         return this._family;
@@ -107,7 +145,7 @@ export class Card extends AssetBase{
         return this._number;
     }
     /**
-     * @description Family (Ace, Spade...) of the current card
+     * @description Family (Club, Spade...) of the current card
      */    
     private readonly _family : CardType;
     
